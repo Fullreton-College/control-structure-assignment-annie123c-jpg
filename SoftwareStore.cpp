1d572911b488c042c7713749a22dd0bd551a2734
Chapman, Anne <annie123c@gmail.com>
2026-03-03 19:23:32 -0800

    Code Written (FIXED)

--- SoftwareStore.cpp
+++ SoftwareStore.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+using std::cout;
+
+//originally uploaded to wrong assignment - FIXED resubmission
+//i was wondering why it said to use a switch statement when it made
+//the code more complicated, but I realize now that it was for the 
+//other assignment (about phone service rather than software store)
+
 int main()
 {
-    double mph = 6.5;
-    // PACE calculation
-    /* write your code */
+    int units;
+    double price = 199.00;
+    double discountPercent = 0.00;
 
-    //express pace in minutes and seconds
-  /* write your code here */
+    cout << "Enter the number of units purchased: ";
+    cin >> units;
 
-  //express your output in 1 decimal points
+    if (units >= 10 && units <= 19)
+    {
+        discountPercent = 0.20;
+    }
+    // changed second discount to be between 20 and 49 units
+    // because it doesn't make sense for units between 40
+    // and 50 to have no discount :) 
+    else if (units >= 20 && units <= 49)
+    {
+        discountPercent = 0.30;
+    }
+    else if (units >= 50 && units <= 99)
+    {
+        discountPercent = 0.40;
+    }
+    else if (units >= 100)
+    {
+        discountPercent = 0.50;
+    }
+    else 
+    {
+        discountPercent = 0.00;
+    }
 
+    double totalCost = units * price * (1 - discountPercent);
+    cout << "Total cost: $" << fixed << setprecision(2) << totalCost << endl;
+
+    return 0;
 
 }
+
